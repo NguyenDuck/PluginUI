fe25a48b2df725b4003d80c8d6a3c8f915d07c3a
NguyenDuck <duck.duc.vit@gmail.com>
2021-09-11 17:41:35 +0700

    Disable plugins command

--- src/NguyenDuck/PluginUI/Main.php
+++ src/NguyenDuck/PluginUI/Main.php
@@ -16,6 +16,23 @@ use pocketmine\plugin\PluginBase;
 class Main extends PluginBase
 {
 	public function onEnable() {
-		
+		$this->unregister("plugins");
+	}
+
+	/**
+	 * @param string
+	 * @return void
+	 */
+	public function unregister(string ...$commands):void {
+		$commandMap = $this->getServer()->getCommandMap();
+		foreach ($commands as $command) {
+			$command = $commandMap->getCommand($command);
+			if (!is_null($command)) {
+				$command->setLabel("§c".$command."_disabled");
+				if ($commandMap->unregister($command)) {
+					$this->getLogger()->notice("Disabled Command ".$command);
+				}
+			}
+		}
 	}
 }
